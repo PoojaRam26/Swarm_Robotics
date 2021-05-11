739bbb77245ff2d066fa05344b329466434e2cfe
PoojaRam26 <poojasrm3@gmail.com>
2021-05-12 00:06:20 +0530

    Final project Upload

---
+++ Swarm_slave.c
@@ -0,0 +1,34 @@
+// Including Necessary files
+
+#include <reg52.h>
+#include <stdio.h>
+
+#define lt 0x06;
+#define rt 0x09;
+#define st 0x0a;
+#define stop 0x00;
+
+unsigned int a;
+void main()
+{
+	while(1)
+	{
+		a=P0&0x0f;
+		if(a==0x06)
+		{
+			P2=lt;
+		}
+		if(a=0x09)
+		{
+			P2=rt;
+		}
+		if(a=0x0a)
+		{
+			P2=st;
+		}
+		if(a=0x00)
+		{
+			P2=stop;
+		}
+	}
+}
